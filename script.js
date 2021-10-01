9a8fb3ed6ee28c0144b7b80a972d72f7d5c4d895
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2021-10-01 18:52:28 +0900

    warota

--- script.js
+++ script.js
@@ -40,3 +40,50 @@ $(function(){
             },duration);
         });
 });
+
+var beforePos = 0;//スクロールの値の比較用の設定
+
+//スクロール途中でヘッダーが消え、上にスクロールすると復活する設定を関数にまとめる
+function ScrollAnime() {
+    var elemTop = $('body').offset().top;
+	var scroll = $(window).scrollTop();
+    //ヘッダーの出し入れをする
+    if(scroll == beforePos) {
+		//IE11対策で処理を入れない
+    }else if(elemTop > scroll || 0 > scroll - beforePos){
+		//ヘッダーが上から出現する
+		$('.nav_container').removeClass('UpMove');	//#headerにUpMoveというクラス名を除き
+		$('.nav_container').addClass('DownMove');//#headerにDownMoveのクラス名を追加
+    }else {
+		//ヘッダーが上に消える
+        $('.nav_container').removeClass('DownMove');//#headerにDownMoveというクラス名を除き
+		$('.nav_container').addClass('UpMove');//#headerにUpMoveのクラス名を追加
+    }
+    
+    beforePos = scroll;//現在のスクロール値を比較用のbeforePosに格納
+}
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
+});
+
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});
+
